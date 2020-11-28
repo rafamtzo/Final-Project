41b3b7ccf2e3fb47a2cea0adf984327b8a4285dc
Jorge Elizondo <j.elizondo126@gmail.com>
2020-11-28 03:55:20 -0600

    Adds Navigation Routes. Note: we need to run the server to navigate through the website.

--- server/app.js
+++ server/app.js
@@ -1,25 +1,21 @@
 const { verify } = require("crypto");
 const express = require("express");
-const verifyToken = require('./middleware/authMiddleware');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const cookieParser = require('cookie-parser')
-const path = require("path");
+
 
 const authRoutes = require('./routes/auth');
 const serviceRotutes = require('./routes/service');
-app.use(cookieParser());
+const navigationRoutes = require('./routes/navigation');
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/',verifyToken,(req, res) => {
-    res.sendFile(path.resolve('./index.html'));
-});
-
 app.use("/", authRoutes);
 app.use("/api/services", serviceRotutes);
-
+app.use("/",navigationRoutes);
 
 app.listen(PORT, function() {
     console.log("Server Listening at: " + PORT);
