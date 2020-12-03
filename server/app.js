aa0eb5f69fbcb782c38aae7c769bc83de48abfe9
Jorge Elizondo <j.elizondo126@gmail.com>
2020-12-03 03:17:04 -0600

    Fixes Cookie bug

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const { verify } = require("crypto");
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 const cookieParser = require('cookie-parser')
 
 
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -28,9 +28,10 @@ exports.login = async (req, res) => {
             const token = jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN});
             console.log(token);
             res.cookie("token", token, {httpOnly:true});
+            res.redirect('/');
         } else {
             console.log("Password is invalid");
-            res.json(false);   
+            res.json(false); 
         }
     }
 }
